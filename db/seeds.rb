bc1e4412f94686127d9ba3c9bc3ed294dbb23f59
Ricardo Pontes <ricardo@connectfood.com.br>
2017-08-10 16:30:06 -0300

    Included more account on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 source_account = Account.create
 destination_account = Account.create
 
-Trade.create(account: source_account, amount: - 10)
-Trade.create(account: destination_account, amount: 10)
+Trade.create(account: source_account, amount: - 100)
+Trade.create(account: destination_account, amount: 100)
+
+source_account = destination_account
+destination_account = Account.create
+
+Trade.create(account: source_account, amount: - 50)
+Trade.create(account: destination_account, amount: 50)
